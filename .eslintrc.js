edc4d413abbeeaa41b77a870fe3355d6765b6712
NikshayReddy <nikshaykotamreddy@gmail.com>
2025-03-08 21:35:22 +0530

    updating

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  extends: ['react-app'],
+  plugins: ['react'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  rules: {
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off'
+  }
+}; 
